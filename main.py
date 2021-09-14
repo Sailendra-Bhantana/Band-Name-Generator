bc263edbadb46bcb92d1707e74d416ca6bc7ae95
Sailendra Bhantana <67456565+sailendra01@users.noreply.github.com>
2021-09-14 22:25:26 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-#1. Create a greeting for your program.
 print("Welcome to the Band Name Generator\n")
-#2. Ask the user for the city that they grew up in.
+
 City = input("What's the name of the city you grew up in?\n")
 
 Pet = input("What's the name of your first pet?\n")
